74e4e4cd838210df3b062e2293186ce449ea06fe
Alexander Beck <beckhv2@gmail.com>
2023-08-31 15:09:43 -0800

    created basic color scheme and layout

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ Email: beckhv2@gmail.com
 Github: https://github.com/bexcoding
 */
 
+const grid = document.getElementById('game-grid');
+const gridSize = 10;
+
+for(i = 0; i < (gridSize * gridSize); i++) {
+    let square = document.createElement('div');
+    square.setAttribute('class', 'game-tile');
+    square.setAttribute('id', i);
+    grid.appendChild(square);
+}
