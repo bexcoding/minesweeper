ca094854edbe2219dc78bd1b4ef82766386d1bc2
Alexander Beck <beckhv2@gmail.com>
2023-08-31 14:01:13 -0800

    added html, css, and js files; linked them together

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# minesweeper
+# Minesweeper
+
 Game in the style of classic minesweeper.
+
+# Functionality
+
+# Process
+
+- create index, readme, style, and script and link them together
---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+Title: Minesweeper
+Description: Classic game of minesweeper for browser
+Last Updated: Aug 31, 2023
+Developer: Alexander Beck
+Email: beckhv2@gmail.com
+Github: https://github.com/bexcoding
+*/
+
